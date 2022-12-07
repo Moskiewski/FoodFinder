e6247d10514e3ff0a0ecf7995c20654933ad45fd
Mateusz Zagrzejewski <myemail@myemail.com>
2022-12-07 11:59:27 +0000

    First commit

---
+++ src/main/java/pl/foodfinder/FoodFinderConsoleApplication.java
@@ -0,0 +1,7 @@
+package pl.foodfinder;
+
+public class FoodFinderConsoleApplication {
+    public static void main(String[] args) {
+
+    }
+}
---
+++ src/test/java/pl/foodfinder/FoodFinderApplicationTest.java
@@ -0,0 +1,23 @@
+package pl.foodfinder;
+
+
+import org.junit.jupiter.api.Test;
+
+public class FoodFinderApplicationTest {
+
+    @Test
+    void ShouldReturnReceipWhenUserGaveFiveCorrectIndrgidiens () {
+
+    }
+
+    @Test
+    void ShouldNotReturnReceipWhenUserGaveAtLeastOneInorrectIndrgidient () {
+
+    }
+
+    @Test
+    void ShouldNotReturnReceipWhenUserGaveMoreThanFiveIndrgidiens () {
+
+    }
+
+}
